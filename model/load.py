873ce19b07500dae1dc13558908d61072785feaa
aponskk <azhzha.lol@mail.ru>
2025-12-11 21:00:21 +0300

    Update load.py

--- model/load.py
+++ model/load.py
@@ -1,19 +1,9 @@
-import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-def load_model(model_path='model_weights.pth'):
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    
-    model = AutoModelForSequenceClassification.from_pretrained(
-        'SkolkovoInstitute/russian_toxicity_classifier',
-        num_labels=2
-    )
-    
-    model.load_state_dict(torch.load(model_path, map_location=device))
-    
-    model.to(device)
+REPO_ID = "aponskk/toxicAI" 
+
+def load_model():
+    model = AutoModelForSequenceClassification.from_pretrained(REPO_ID)
+    tokenizer = AutoTokenizer.from_pretrained(REPO_ID)
     model.eval()
-    
-    tokenizer = AutoTokenizer.from_pretrained('SkolkovoInstitute/russian_toxicity_classifier')
-    
     return model, tokenizer
